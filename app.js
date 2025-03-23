553b00a9a0b098a4cf341120640675dcaa6083d7
JauniKappauni <social.jauni@gmail.com>
2025-03-23 17:24:19 +0100

    Added file upload route with multer and handle single file upload

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const multer = require('multer')
+const uploads = multer({dest:'./public/uploads/'})
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req,res) => {
     res.render('index')
+})
+
+app.post('/upload', uploads.single('file'), function(req,res){
+    res.send(`${req.file.originalname} was uploaded`)
+    console.log(`${req.file.originalname} was uploaded`)
 })
 
 app.listen(port, () => console.log(`Der Server läuft nun auf http://localhost:${port}`))
